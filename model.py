2aef56b547ba3d9e507d85b21b5f5a73143551d0
Muzna <91617542+MSMALG@users.noreply.github.com>
2024-11-14 19:03:47 +0300

    Update model.py

--- model.py
+++ model.py
@@ -6,39 +6,31 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
 import joblib
 
-# Load the processed dataset
 df = pd.read_csv("processed_bioactivity_data.csv")
 
-# Features to use (excluding the target variable)
+#Features to use (excluding the target variable)
 X = df[['MoleculeWeight', 'LogP', 'NumHDonors', 'NumHAcceptors', 'pIC50']]
 
-# Target variable (bioactivity classification: active vs inactive)
-# We'll need to encode the bioactivity class into numeric values (active=1, inactive=0)
+#Target variable (bioactivity classification: active vs inactive)
 le = LabelEncoder()
 df['bioactivity'] = le.fit_transform(df['bioactivity'])
 
-y = df['bioactivity']  # Target variable (encoded as 1 for active, 0 for inactive)
+y = df['bioactivity']  
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize the ExtraTreesRegressor model
 model = ExtraTreesRegressor(n_estimators=100, random_state=42)
 
-# Train the model
 model.fit(X_train, y_train)
 
-# Make predictions
 y_pred = model.predict(X_test)
 
-# Evaluate the model (you can use accuracy, RMSE, or any other metric)
+# Evaluate the model 
 mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Squared Error: {mse}')
 
-# Save the trained model using joblib
 joblib.dump(model, 'extra_trees_model.pkl')
 print('Model saved as extra_trees_model.pkl')
 
-# Optionally, save the label encoder for future use (to reverse the encoding of predictions)
 joblib.dump(le, 'label_encoder.pkl')
 print('Label Encoder saved as label_encoder.pkl')
